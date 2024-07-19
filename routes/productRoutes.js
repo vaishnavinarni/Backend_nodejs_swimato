b6cf78ac6d7e86977c58a6c8752f27b23256e661
unknown <vaishnavi.n5b5@gmail.com>
2024-07-19 23:30:05 +0530

    first commit

---
+++ routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const productController=require("../controllers/productController");
+const router=express.Router();
+
+router.post('/add-product/:firmId',productController.addProduct);
+router.get('/:firmId/products',productController.getProductByFirm);
+
+router.get('/uploads/:imageName',(req,res)=>{
+    const imageName=req.params.imageName;
+    req.headersSent('content-type','image/jpeg');
+    res.sendFile(path.join(__dirname,'..','uploads',imageName));
+});
+
+router.delete('/:productId',productController.deleteProductById);
+
+module.exports=router;
