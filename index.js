247c9f96aecaaef744dbe670d53617689366cd5c
unknown <vaishnavi.n5b5@gmail.com>
2024-07-30 12:07:05 +0530

    added path module

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require("express");
-const dotEnv=require('dotenv');
-const mongoose=require('mongoose');
-const vendorRoutes=require('./routes/vendorRoutes');
-const bodyParser=require('body-parser');
-const firmRoutes=require('./routes/firmRoutes')
-const productRoutes=require('./routes/productRoutes')
-const path=require('path')
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const vendorRoutes = require('./routes/vendorRoutes');
+const bodyParser = require('body-parser');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
 
-const app = express();
+const app = express()
+
+
 const PORT = process.env.PORT || 4000;
-app.use(express.json());
 dotEnv.config();
+app.use(cors())
+
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB connected successfully"))
-.catch((error)=>console.log(error))
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((error) => console.log(error));
+
 
 app.use(bodyParser.json());
-app.use('/vendor',vendorRoutes);
-app.use('/firm',firmRoutes)
-app.use('/product',productRoutes)
-app.use('/uploads',express.static('uploads'));
-
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes);
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
 
 app.listen(PORT, () => {
     console.log(`Server started and running at ${PORT}`);
 });
+
 
 app.use('/', (req, res) => {
     res.send("<h1> Welcome to SWIMATO</h1>");
